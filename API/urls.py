89b6d5f563aa4864918eaab08acfaaa64ed7443d
unknown <mimick.stern@gmail.com>
2023-07-27 13:53:53 +0300

    index.change

--- API/urls.py
+++ API/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('/', views.index_view, name='index'),
+    path('', views.index_view, name='index'),
     path('signup/', views.signUp, name="signup"),
     path('received-messages/', views.getReceivedMessages, name='received'),
     path('sent-messages/', views.getSentMessages,name="sent"),
